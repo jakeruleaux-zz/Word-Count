8b7e6fe2155b374a5b1326d6cb99bb275fdebf87
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-07 16:24:44 -0700

    build test for three words

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,27 +5,28 @@
 
 
 
-        function countRepeats($input_word, $input_phrase)
+        function countRepeats($input_phrase, $input_word)
         {
             $lower_word = strtolower($input_word);
+            // var_dump($lower_word);
             // return $lower_word;
             $lower_phrase = strtolower($input_phrase);
             $explode_phrase = explode(" ", $lower_phrase);
             // return $lower_phrase;
 
             $word_array = [];
-            var_dump($word_array);
-            foreach ($explode_phrase as $phrase => $word)
+            // var_dump($word_array);
+            foreach ($explode_phrase as $phrase)
              {
-                if ($explode_phrase[$phrase] == $lower_word) {
-
+                if ($phrase == $lower_word) {
                     array_push($word_array, "1");
+// var_dump($phrase);
                 }
 
             }
             $word_total = array_sum($word_array);
             return $word_total;
-            var_dump($word_array);
+            var_dump($word_total);
         }
 
 
--- tests/WordCountTest.php
+++ tests/WordCountTest.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "src/WordCount.php";
+    require_once "src/RepeatCounter.php";
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
 
@@ -23,6 +23,17 @@
             $result = $test_word_count_two->countRepeats($lower_phrase, $lower_word);
 
             $this->assertEquals("2", $result);
+        }
+
+        function testWordCounterThree()
+        {
+            $test_word_count_three = new RepeatCounter;
+            $lower_phrase = "this is a test or test";
+            $lower_word = "test";
+
+            $result = $test_word_count_three->countRepeats($lower_phrase, $lower_word);
+
+            $this->assertEquals("3", $result);
         }
 
     }
