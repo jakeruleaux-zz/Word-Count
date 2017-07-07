91396f8e59658d481a53963ed61ac13d96831535
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-07 14:03:51 -0700

    build test for word count

--- src/WordCount.php
+++ src/WordCount.php
@@ -9,9 +9,17 @@
         {
             $lower_word = strtolower($input_word);
             return $lower_word;
-
             $lower_phrase = strtolower($input_phrase);
             return $lower_phrase;
+
+            // $word_array = [];
+            foreach ($lower_phrase as $word) {
+                if ($lower_phrase == $word) {
+                    return $word;
+                }
+
+            }
+
         }
 
 
--- tests/WordCountTest.php
+++ tests/WordCountTest.php
@@ -4,30 +4,24 @@
     {
         function testWordLower()
         {
-            $test_word_lower= new RepeatCounter;
+            $test_word_lower = new RepeatCounter;
             $lower_word = "WORD";
 
             $result = $test_word_lower->countRepeats($lower_word);
+
             $this->assertEquals("word", $result);
         }
 
-        // function testphraseLower()
-        // {
-        //     $test_phrase_lower= new RepeatCounter;
-        //     $lower_phrase = "THIS IS A TEST";
-        //     $result = $test_phrase_lower->countRepeats($lower_phrase, $lower_word);
-        //     $this->assertEquals("this is a test", $result);
-        // }
+        function testWordCounter()
+        {
+            $test_word_count = new RepeatCounter;
+            $lower_word = "test";
+            $lower_phrase = "this is a test";
 
-        // function testWordCount()
-        // {
-        //     $test_word_count= new RepeatCounter;
-        //     $lower_phrase = "this is a test or test";
-        //     $result = $test_word_count->countRepeats($lower_phrase);
-        //     $this->assertEquals("2", $result);
-        // }
+            $result = $test_word_count->countRepeats($lower_word, $lower_phrase);
 
-
+            $this->assertEquals("1", $result);
+        }
 
     }
 ?>
