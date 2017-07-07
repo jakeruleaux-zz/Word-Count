3e7880ff2b7a8051ee51e11e70ddc7d10fe8ad90
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-07 13:53:40 -0700

    refactor and remove poor methods

--- src/WordCount.php
+++ src/WordCount.php
@@ -3,29 +3,15 @@
     class RepeatCounter
     {
 
-        function wordLower($input_word)
+
+
+        function countRepeats($input_word)
         {
             $lower_word = strtolower($input_word);
             return $lower_word;
-        }
-        // function phraseLower($input_phrase)
-        // {
-        // }
 
-        function countRepeats($input_phrase)
-        {
-
-            // $phrase_arr = [];
-            // var_dump($phrase_arr);
             $lower_phrase = strtolower($input_phrase);
-            // array_push($phrase_arr, $lower_phrase);
-            // return $phrase_arr;
-            // $occurrences = (array_count_values($phrase_arr));
-            // var_dump($occurrences);
-            // $test = implode(" ", $occurrences);
-            $test = substr_count($lower_phrase, 'test');
-            return $test;
-            // return $occurrences;
+            return $lower_phrase;
         }
 
 
--- tests/WordCountTest.php
+++ tests/WordCountTest.php
@@ -5,26 +5,27 @@
         function testWordLower()
         {
             $test_word_lower= new RepeatCounter;
-            $input = "WORD";
-            $result = $test_word_lower->wordLower($input);
+            $lower_word = "WORD";
+
+            $result = $test_word_lower->countRepeats($lower_word);
             $this->assertEquals("word", $result);
         }
 
-        function testphraseLower()
-        {
-            $test_phrase_lower= new RepeatCounter;
-            $input = "THIS IS A TEST";
-            $result = $test_phrase_lower->countRepeats($input);
-            $this->assertEquals("this is a test", $result);
-        }
+        // function testphraseLower()
+        // {
+        //     $test_phrase_lower= new RepeatCounter;
+        //     $lower_phrase = "THIS IS A TEST";
+        //     $result = $test_phrase_lower->countRepeats($lower_phrase, $lower_word);
+        //     $this->assertEquals("this is a test", $result);
+        // }
 
-        function testWordCount()
-        {
-            $test_word_count= new RepeatCounter;
-            $input = "this is a test or test";
-            $result = $test_word_count->countRepeats($input);
-            $this->assertEquals("2", $result);
-        }
+        // function testWordCount()
+        // {
+        //     $test_word_count= new RepeatCounter;
+        //     $lower_phrase = "this is a test or test";
+        //     $result = $test_word_count->countRepeats($lower_phrase);
+        //     $this->assertEquals("2", $result);
+        // }
 
 
 
