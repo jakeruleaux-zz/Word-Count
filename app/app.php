851ca1707f32257f472b6e830e685a771a9c6eea
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-07 21:59:46 -0700

    debug result twig file in control

--- app/app.php
+++ app/app.php
@@ -2,7 +2,14 @@
     date_default_timezone_set("America/Los_Angeles");
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
+
+    use Symfony\Component\Debug\Debug;
+Debug::enable();
+
     $app = new Silex\Application();
+
+    $app['debug'] = true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
@@ -11,10 +18,10 @@
           return $app['twig']->render('form.html.twig');
       });
 
-      $app->get("/count", function() use ($app){
+      $app->get("/count", function() use ($app) {
           $final_count = new RepeatCounter;
-          $the_outcome = $final_count->countRepeats($_GET['input_phrase'], ['input_word']);
-          return $app['twig']->render('score.html.twig', array('result' => $the_outcome));
+          $the_outcome = $final_count->countRepeats($_GET['input_phrase'], $_GET['input_word']);
+          return $app['twig']->render('count.html.twig', array('result' => $the_outcome));
       });
 
 
